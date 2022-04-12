537b1f9ce7747bd1e7ac675d7ed3dea01b96dc41
Рамазан Сраждинов <srazhdinovr@bk.ru>
2022-04-12 10:50:55 +0300

    creating city and distance services

---
+++ distance-calculator/src/main/java/com/example/distancecalculator/services/CityService.java
@@ -0,0 +1,4 @@
+package com.example.distancecalculator.services;
+
+public interface CityService {
+}
---
+++ distance-calculator/src/main/java/com/example/distancecalculator/services/CityServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.distancecalculator.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CityServiceImpl implements CityService {
+}
