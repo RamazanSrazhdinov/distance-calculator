a714ab0a3879abdd7f9fcee7208089d1daf06341
Рамазан Сраждинов <srazhdinovr@bk.ru>
2022-04-12 10:58:42 +0300

    add getMapping for getCities

--- distance-calculator/src/main/java/com/example/distancecalculator/controllers/CityController.java
+++ distance-calculator/src/main/java/com/example/distancecalculator/controllers/CityController.java
@@ -1,8 +1,25 @@
 package com.example.distancecalculator.controllers;
 
+import com.example.distancecalculator.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cities")
 public class CityController {
+    private final CityService cityService;
+
+    @Autowired
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @GetMapping
+    public ResponseEntity getCities(){
+        return ResponseEntity.ok("ok");
+    }
 }
